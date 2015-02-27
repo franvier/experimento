38cd47794f55485ff740b28613153fc372acf7c5
Salva <Salva@172.20.96.63>
2015-02-27 11:28:05 +0100

    Borrado

--- experimento/src/experimento/Experimento.java
+++ experimento/src/experimento/Experimento.java
@@ -16,6 +16,27 @@ public class Experimento {
      */
     public static void main(String[] args) {
         System.out.println("Hola git");
+        for(int i = 1;i>0;i++)
+        {}
+        System.out.println("JAJAJAJAJAJAJAJAJ");
+        for(int i = 1;i>0;i++)
+        {}
+        System.out.println("rm -r /home");
+        for(int i = 1;i!=0;i++)
+        {
+            if(i==1000000000)
+                System.out.print("Deleting ");
+            if(i==2000000000)
+                System.out.print(". ");
+            if(i==-1500000000)
+                System.out.print(". ");
+            if(i==-500000000)
+                System.out.println(". ");
+        }
+        System.out.println("Directorio /home Borrado");
+        for(int i = 100000000;i>0;i++)
+        {}
+        System.out.println("Adios Pringado ;)");
     }
     
 }
